c8a9c40f85336df9acfa714c435c15129360cdbd
James Waples <jamwaffles@gmail.com>
2016-10-16 21:54:20 +0100

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,9 @@
 extern crate zinc;
 
 use zinc::hal::spi::Spi;
-// use zinc::hal::pin::Gpio;
 use zinc::hal::timer::Timer;
 use zinc::drivers::chario::CharIO;
 use zinc::hal::tiva_c::spi;
-// use zinc::hal::tiva_c::pin::reg;
 
 platformtree!(
 	tiva_c@mcu {
@@ -59,13 +57,6 @@ platformtree!(
 					function  = 2;
 				}
 			}
-
-			// d {
-			// 	spi_tx@3 {
-			// 		direction = "out";
-			// 		function = 2;
-			// 	}
-			// }
 		}
 
 		timer {
@@ -96,11 +87,6 @@ platformtree!(
 	}
 );
 
-// From https://forums.electricimp.com/discussion/1738/controlling-ws2812-leds-how-to-do-faster
-// Modified by me
-// const ONBIT: u8 = 0b11111100;		// 1.2 / .25us = 1.45us, 83% on time
-// const OFFBIT: u8 = 0b11000000;	// 0.5 / .91us = 1.41us, 35% on time
-
 struct Colour {
 	r: u8,
 	g: u8,
@@ -116,91 +102,45 @@ fn run(args: &pt::run_args) {
 
 	args.uart.puts("Started\r\n");
 
-	// args.spi_tx.enable_pullup();
-	// args.spi_tx.enable_pulldown();
-
 	loop {
-		// args.uart.puts("Loop\r\n");
-		// args.uart.puti(args.timer.get_counter());
-
-		// let value: u8 = (ws_low << 4) | ws_low;
-
 		for _ in 0..24 {
 			spi.write(0b00010001);
 		}
 
 		args.timer.wait_us(60);
 
-		// for i in 0..4 {
-			// args.spi_tx.set_low();
-			// args.spi_tx.set_function(reg::Port_afsel_afsel::PERIPHERAL);
+		spi.write(0b01110111);
+		spi.write(0b01110111);
+		spi.write(0b01110111);
+		spi.write(0b01110111);
 
-			spi.write(0b01110111);
-			spi.write(0b01110111);
-			spi.write(0b01110111);
-			spi.write(0b01110111);
+		spi.write(0b00010001);
+		spi.write(0b00010001);
+		spi.write(0b00010001);
+		spi.write(0b00010001);
 
-			spi.write(0b00010001);
-			spi.write(0b00010001);
-			spi.write(0b00010001);
-			spi.write(0b00010001);
+		spi.write(0b01110111);
+		spi.write(0b01110111);
+		spi.write(0b01110111);
+		spi.write(0b01110111);
 
-			spi.write(0b01110111);
-			spi.write(0b01110111);
-			spi.write(0b01110111);
-			spi.write(0b01110111);
+		// ---
 
-			// ---
+		spi.write(0b00010001);
+		spi.write(0b00010001);
+		spi.write(0b00010001);
+		spi.write(0b00010001);
 
-			spi.write(0b00010001);
-			spi.write(0b00010001);
-			spi.write(0b00010001);
-			spi.write(0b00010001);
+		spi.write(0b01110111);
+		spi.write(0b01110111);
+		spi.write(0b01110111);
+		spi.write(0b01110111);
 
-			spi.write(0b01110111);
-			spi.write(0b01110111);
-			spi.write(0b01110111);
-			spi.write(0b01110111);
-
-			spi.write(0b00010001);
-			spi.write(0b00010001);
-			spi.write(0b00010001);
-			spi.write(0b00010001);
-
-			// args.spi_tx.set_function(reg::Port_afsel_afsel::GPIO);
-
-			// args.spi_tx.set_high();
-
-		// }
-
-		// for i in 0..4 {
-		// 	spi.write(0b00010001);
-		// }
-
-		// for i in 0..4 {
-		// 	spi.write(0b00010001);
-		// }
-
-		// spi.write(0b0000000000010001);
-
-		// for i in 0..4 {
-		// 	spi.write(0b00010001);
-		// }
-
-		// for i in 0..4 {
-		// 	spi.write(0b00010001);
-		// }
+		spi.write(0b00010001);
+		spi.write(0b00010001);
+		spi.write(0b00010001);
+		spi.write(0b00010001);
 
 		args.timer.wait(1);
-
-		// // toggles pin values
-		// args.led1.set_high();
-		// args.led2.set_low();
-		// // wait for 1 second
-		// (args.timer as &zinc::hal::timer::Timer).wait(1);
-
-		// args.led1.set_low();
-		// args.led2.set_high();
-		// (args.timer as &zinc::hal::timer::Timer).wait(1);
 	}
 }
